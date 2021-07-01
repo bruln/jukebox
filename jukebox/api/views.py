8186c8ef73485dad2a040e74c7e9d65efdb811db
bru <brunodlenza@gmail.com>
2021-07-01 16:54:24 -0500

    second commit

--- jukebox/api/views.py
+++ jukebox/api/views.py
@@ -1,8 +1,11 @@
+from .models import Room
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
 
 # Create your views here.
 
 
-def main(request):
-    return HttpResponse("<h1>Welcome<h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
