06ea2ba7c2fbc335c250d99bf62834539b193dda
bru <brunodlenza@gmail.com>
2021-07-01 18:33:45 -0500

    fixed file structure issues

--- jukebox/frontend/apps.py
+++ jukebox/frontend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FrontedConfig(AppConfig):
+class FrontendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fronted'
+    name = 'frontend'
